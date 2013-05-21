c5ca8d03c6c6611a497b793c1da31b6fe65c8f3f
quyen <nttqa22001@yahoo.com>
2013-05-21 19:13:46 +0200

    fix loading problem

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,21 @@
-ActionController::Routing::Routes.draw do |map|
-  map.resources :posts, :name_prefix => 'all_', :collection => { :search => :get }
-	map.resources :forums, :topics, :posts, :monitorship
-
-  %w(forum).each do |attr|
-    map.resources :posts, :name_prefix => "#{attr}_", :path_prefix => "/#{attr.pluralize}/:#{attr}_id"
-  end
-  
-  map.resources :forums do |forum|
-    forum.resources :topics do |topic|
-      topic.resources :posts
-      topic.resource :monitorship, :controller => :monitorships
+Rails.application.routes.draw do
+  resources :forums do
+    resources :topics do
+      resources :posts
+      resource :monitorship, :controller => :monitorships
     end
   end
 
-# screws up default / defined by the application    
-#  map.forum_home '', :controller => 'forums', :action => 'index'
+  resources :posts, :as => "all_posts" do
+    collection do
+      get :search
+    end
+  end
+
+  %w(forum).each do |attr|
+    resources :posts, :as => "#{attr}_posts", :path_prefix => "/#{attr.pluralize}/:#{attr}_id"
+  end
+
+  resources :topics
+  resources :monitorship
 end
