d8e2af89ae70cbf336b5ef3168d50c69ab77b580
quyen <nttqa22001@yahoo.com>
2013-06-05 17:32:25 +0200

    fix RAILS_ROOT

--- app/models/posts_sweeper.rb
+++ app/models/posts_sweeper.rb
@@ -2,10 +2,10 @@ class PostsSweeper < ActionController::Caching::Sweeper
   observe Post
   
   def after_save(post)
-    FileUtils.rm_rf File.join(RAILS_ROOT, 'public', 'forums', post.forum_id.to_s, 'posts.rss')
-    FileUtils.rm_rf File.join(RAILS_ROOT, 'public', 'forums', post.forum_id.to_s, 'topics', "#{post.topic_id}.rss")
-    FileUtils.rm_rf File.join(RAILS_ROOT, 'public', 'users')
-    FileUtils.rm_rf File.join(RAILS_ROOT, 'public', 'posts.rss')
+    FileUtils.rm_rf File.join(Rails.root, 'public', 'forums', post.forum_id.to_s, 'posts.rss')
+    FileUtils.rm_rf File.join(Rails.root, 'public', 'forums', post.forum_id.to_s, 'topics', "#{post.topic_id}.rss")
+    FileUtils.rm_rf File.join(Rails.root, 'public', 'users')
+    FileUtils.rm_rf File.join(Rails.root, 'public', 'posts.rss')
   end
   
   alias_method :after_destroy, :after_save
